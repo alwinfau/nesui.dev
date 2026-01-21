7a80f588eab20de04c38a6472760a89a8abe580a
Alwin Fau <alwinfau.official@gmail.com>
2026-01-21 21:08:36 +0700

    wip

---
+++ lib/component/spinner.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+
+class NesuiSpin extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+
+  const NesuiSpin({
+    super.key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 900),
+  });
+
+  @override
+  State<NesuiSpin> createState() => _NesuiSpinState();
+}
+
+class _NesuiSpinState extends State<NesuiSpin>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _c = AnimationController(
+    vsync: this,
+    duration: widget.duration,
+  )..repeat();
+
+  @override
+  void dispose() {
+    _c.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(turns: _c, child: widget.child);
+  }
+}
